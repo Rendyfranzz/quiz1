58460dafc8957044c83844e236e1f9397ca81931
Rendi <rendicrew96@gmail.com>
2022-09-21 10:15:22 +0700

    update

--- assets/main.js
+++ assets/main.js
@@ -8,15 +8,8 @@ const form = document.getElementById("form")
 const lomba = document.getElementsByName("lomba")
 
 form.addEventListener("submit",function(){
-    // for (var i = 0, length = lomba.length; i < length; i++) {
-    //     if (lomba[i].checked) {
-    //       alert(lomba[i].value);
-    //       break;
-    //     }
-    //   }
-    if(nama.value != null && utusan.value != null && kelurahan.value != null && kecamatan.value != null && telepon.value != null ){
-        window.alert("berhasil submit")
-    }else{ window.alert("periksa lagi")}
+    
+    window.alert("berhasil submit")
    
 })
 
